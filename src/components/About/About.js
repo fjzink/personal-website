8f85ea0329fec5da52fae27bf0bc4e4ca5c52f8e
Fulton Zink <joeyzink@gmail.com>
2018-02-22 16:34:19 -0800

    Add styling for profile picture,

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import './About.css';
 
 class About extends Component {
   render() {
     return(
       <div className="About">
-        <img src={require('./joey.png')} alt={"joey"} />
+        <img src={require('./joey.png')} alt={"joey"} className={"profile-pic"}/>
       </div>
     );
   }
