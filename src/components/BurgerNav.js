130b2e864ccec323cc9037b84b30d7466c6df3e5
Fulton Zink <joeyzink@gmail.com>
2018-02-27 13:20:53 -0800

    Add home link to take the user back to the top of the page.

--- src/components/BurgerNav.js
+++ src/components/BurgerNav.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { stack as Menu } from 'react-burger-menu';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import { faEnvelope, faUser, faBriefcase, faListUl } from '@fortawesome/fontawesome-free-solid/';
+import { faEnvelope, faUser, faBriefcase, faListUl, faHome } from '@fortawesome/fontawesome-free-solid/';
 import './BurgerNav.css';
 
 class BurgerNav extends Component {
@@ -20,6 +20,7 @@ class BurgerNav extends Component {
   render() {
     return(
       <Menu isOpen={this.state.menuOpen} >
+        <a onClick={() => this.closeMenu()} className={"nav-link"} href={"#top"}><FontAwesomeIcon icon={faHome} /> Home</a>
         <a onClick={() => this.closeMenu()} className={"nav-link"} href={"#about-section"}><FontAwesomeIcon icon={faUser} /> About</a>
         <a onClick={() => this.closeMenu()} className={"nav-link"} href={"#skills-section"}><FontAwesomeIcon icon={faListUl} /> Skills</a>
         <a onClick={() => this.closeMenu()} className={"nav-link"} href={"#projects-section"}><FontAwesomeIcon icon={faBriefcase} /> Projects</a>
