27482f91b8cd49d63feb62fe0bb0fcaec0db234b
Fulton Zink <joeyzink@gmail.com>
2018-02-28 12:27:17 -0800

    Add animation to header.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,8 +5,8 @@ class Header extends Component {
   render() {
     return(
       <div className="Header">
-        <h1 className={"name-title"}>Joey Zink</h1>
-        <img src={require('./joey.png')} alt={"joey"} className={"profile-pic"}/>
+        <h1 className={"name-title animated bounceInLeft"}>Joey Zink</h1>
+        <img src={require('./joey.png')} alt={"joey"} className={"profile-pic animated bounceInLeft"}/>
       </div>
     );
   }
