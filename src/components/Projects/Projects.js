4c7fa2d6b4e796e177536134d555d64db358cc7f
Fulton Zink <joeyzink@gmail.com>
2018-03-02 13:34:20 -0800

    Add more projects. Add flexbox css styling to adjust how the projects are displayed on mobile and desktop.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,7 +3,10 @@ import SectionTitle from '../SectionTitle/SectionTitle';
 import Project from '../Project/Project';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import { faBriefcase } from '@fortawesome/fontawesome-free-solid/';
-import rwpic from './roadwarriors.png'
+import rwpic from './roadwarriors.png';
+import espic from './eliteskills.png';
+import websitepic from './mywebsite.jpg';
+import nigelpic from './nigel.png';
 import './Projects.css';
 
 class Projects extends Component {
@@ -11,9 +14,7 @@ class Projects extends Component {
     super(props);
 
     this.state = {
-      projects: [
-        {
-          title: "RoadWarriors",
+        roadwarriors: {
           description: [
             "Roadwarriors is a road trip app that helps you find food and gas along your route.",
             "You can search for a destination and plot your route.",
@@ -22,8 +23,35 @@ class Projects extends Component {
             "Multiple APIs were used to get data including the Google Maps API, Google Places API, and the Yelp API."
           ],
           image: rwpic
+        },
+        eliteskills: {
+          description: [
+            "Elite Skills is a web app designed to help you improve any skill you want to track.",
+            "Skills that are related are organized into groups.",
+            "You can enter in your progress each day for a particular skill, and you can view data visualization to help you gauge your progress.",
+            "The frontend user interface is built using React, and the backend is a JSON API built using Ruby on Rails.",
+            "Data visualization for skills is created using the Victory library by Formidable, and the UI was made using Bootstrap."
+          ],
+          image: espic
+        },
+        thiswebsite: {
+          description: [
+            "This is my personal webite that I created using React.",
+            "I used Font Awesome's icons as well as their React library to add the SVG icons to my site.",
+            "The navigation menu was created using the react-burger-menu npm module.",
+            "The Animate.css library was used to add animations to the header section of the site."
+          ],
+          image: websitepic
+        },
+        nigel: {
+          description: [
+            "Nigel is web app designed for use in education that allows people to share resources.",
+            "Resources can be tagged with a hashtag so that they can be categorized.",
+            "There are teacher and student user accounts each with different levels of authorization.",
+            "The web app was created using Ruby on Rails and uses PostgreSQL for the database."
+          ],
+          image: nigelpic
         }
-      ]
     };
   }
 
@@ -31,18 +59,33 @@ class Projects extends Component {
     return(
       <div className="Projects" id="projects-section">
         <SectionTitle><FontAwesomeIcon icon={faBriefcase} /> Projects</SectionTitle>
-        {
-          this.state.projects.map((thing) => {
-            return(
-              <Project
-                title={thing.title}
-                image={thing.image}
-                description={thing.description}
-              />
-            );
-          })
-        }
-
+        <div className={"project-rows"}>
+          <div className={"first-row"}>
+            <Project
+              title={"RoadWarriors"}
+              image={this.state.roadwarriors.image}
+              description={this.state.roadwarriors.description}
+            />
+            <Project
+              id={"eliteskills"}
+              title={"Elite Skills"}
+              image={this.state.eliteskills.image}
+              description={this.state.eliteskills.description}
+            />
+          </div>
+          <div className={"second-row"}>
+            <Project
+              title={"This Website"}
+              image={this.state.thiswebsite.image}
+              description={this.state.thiswebsite.description}
+            />
+            <Project
+              title={"Nigel"}
+              image={this.state.nigel.image}
+              description={this.state.nigel.description}
+            />
+          </div>
+        </div>
       </div>
     );
   }
