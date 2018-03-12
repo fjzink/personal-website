dceaa7e8d8612e596520af7facf13f4c83ae998e
Fulton Zink <joeyzink@gmail.com>
2018-03-11 23:26:41 -0700

    Change header animation to fade.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,9 +5,9 @@ class Header extends Component {
   render() {
     return(
       <div className="Header">
-        <h1 className={"name-title animated bounceInLeft"}>Joey Zink</h1>
-        <img src={require('./joey2.jpg')} alt={"joey"} className={"profile-pic animated bounceInLeft"}/>
-        <p className={"job-title animated bounceInLeft"}>Sofware Engineer</p>
+        <h1 className={"name-title animated fadeInLeftBig"}>Joey Zink</h1>
+        <img src={require('./joey2.jpg')} alt={"joey"} className={"profile-pic animated fadeInLeftBig"}/>
+        <p className={"job-title animated fadeInLeftBig"}>Sofware Engineer</p>
       </div>
     );
   }
